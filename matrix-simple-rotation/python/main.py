4fab4e5430b022ba54afbc8d8deb7165e85d964a
Ben Ripkens <bripkens.dev@gmail.com>
2012-10-29 17:37:33 +0100

    Matrix rotation for the cases width != height

--- matrix-simple-rotation/python/main.py
+++ matrix-simple-rotation/python/main.py
@@ -1,13 +1,14 @@
 def rotate(m):
-  width = len(m)
-  height = len(m[0])
+  height = len(m)
+  width = len(m[0])
 
   newMatrix = [[0] * height for h in range(width)]
 
   for y, r in enumerate(m):
     for x, v in enumerate(r):
-      newX = width - y - 1
+      newX = height - y - 1
       newY =  x
+
       newMatrix[newY][newX] = v
 
   return newMatrix
@@ -33,4 +34,15 @@ if __name__ == '__main__':
       [14, 10, 6, 2],
       [15, 11, 7, 3],
       [16, 12, 8, 4]
+    ]
+
+  assert rotate([
+      [ 1,  2,  3,  4],
+      [ 5,  6,  7,  8],
+      [ 9, 10, 11, 12]
+    ]) == [
+      [ 9, 5, 1],
+      [10, 6, 2],
+      [11, 7, 3],
+      [12, 8, 4]
     ]
