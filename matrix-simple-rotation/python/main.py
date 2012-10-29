9fe93a8ba88cadacc281c398d7af88cf0552af25
Ben Ripkens <bripkens.dev@gmail.com>
2012-10-29 17:41:32 +0100

    Preparation for in-place rotation.

--- matrix-simple-rotation/python/main.py
+++ matrix-simple-rotation/python/main.py
@@ -13,8 +13,8 @@ def rotate(m):
 
   return newMatrix
 
-if __name__ == '__main__':
-  assert rotate([
+def verify(fn):
+  assert fn([
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
@@ -24,7 +24,7 @@ if __name__ == '__main__':
       [9, 6, 3]
     ]
 
-  assert rotate([
+  assert fn([
       [ 1,  2,  3,  4],
       [ 5,  6,  7,  8],
       [ 9, 10, 11, 12],
@@ -36,7 +36,7 @@ if __name__ == '__main__':
       [16, 12, 8, 4]
     ]
 
-  assert rotate([
+  assert fn([
       [ 1,  2,  3,  4],
       [ 5,  6,  7,  8],
       [ 9, 10, 11, 12]
@@ -46,3 +46,6 @@ if __name__ == '__main__':
       [11, 7, 3],
       [12, 8, 4]
     ]
+
+if __name__ == '__main__':
+  verify(rotate)
